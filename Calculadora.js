27af9144555e3c3ceaf335a92309540937b34e7b
Rodrikool <gabii.borges1998@gmail.com>
2021-07-19 13:34:51 -0300

    Añadí la función de restar a la Calculadora.

--- Calculadora.js
+++ Calculadora.js
@@ -1,4 +1,6 @@
 const suma = require("./Suma/suma");
+const resta = require("./Resta/resta");
 
 
 console.log(suma(14,8));
+console.log(resta(20,13));
---
+++ Resta/resta.js
@@ -0,0 +1,10 @@
+function resta(num1,num2){
+    let resultado = num1 - num2
+    return num1 + " - " + num2 + " = " + resultado
+};
+
+//console.log(resta(15,5));
+
+
+
+module.exports = resta
