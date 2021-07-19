1380a7f04f66f5e93987cf914e0a13b50a98e186
Rodrikool <gabii.borges1998@gmail.com>
2021-07-19 13:17:06 -0300

    Creé el archivo Calculadora y su primer función de suma.

---
+++ Calculadora.js
@@ -0,0 +1,4 @@
+const suma = require("./Suma/suma");
+
+
+console.log(suma(14,8));
---
+++ Suma/suma.js
@@ -0,0 +1,10 @@
+function suma(num1,num2){
+    let resultado = num1 + num2
+    return num1 + " + " + num2 + " = " + resultado
+};
+
+//console.log(suma(5,5));
+
+
+
+module.exports = suma
